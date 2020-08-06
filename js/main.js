e259222b657f17247a3b3ed20ff930a3cf089a6d
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-08-06 12:55:54 +0300

    slider added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.hero-slider', {
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hero-slider__btn-next',
+    prevEl: '.hero-slider__btn-prev',
+  },
+
+})
