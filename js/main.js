f58c386c5d17ba328a9b3e45caaeb729ab5cc33e
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-08-06 14:24:03 +0300

    hero section desktop added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var mySwiper = new Swiper('.hero-slider', {
   loop: true,
-
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 30,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hero-slider__btn-next',
-    prevEl: '.hero-slider__btn-prev',
+    nextEl: '.hero-slider__btn--next',
+    prevEl: '.hero-slider__btn--prev',
   },
 
 })
