30e358bc84e6e6629e57c8689b8775ba6740741a
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-08-11 15:03:51 +0300

    hero adaptive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var mySwiper = new Swiper('.hero-slider', {
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1,
   centeredSlides: true,
   spaceBetween: 25,
 
@@ -9,5 +9,16 @@ var mySwiper = new Swiper('.hero-slider', {
     nextEl: '.hero-slider__btn--next',
     prevEl: '.hero-slider__btn--prev',
   },
+
+  noSwipingClass: 'hero-slider',
+
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+      noSwipingClass: 'swiper-no-siping',
+    }
+  }
 
 })
