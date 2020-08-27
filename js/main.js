adf974c38d407d8055a3f594ae654ab1c4af639a
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-08-27 17:00:55 +0300

    cakes section, tabs and first slider

--- js/main.js
+++ js/main.js
@@ -51,3 +51,20 @@ var reviewsSlider = new Swiper('.reviews__slider', {
   }
 
 })
+
+var CakesSliderBiscuit = new Swiper('.cakes__slider--biscuit', {
+  loop: true,
+  slidesPerView: 3,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+})
